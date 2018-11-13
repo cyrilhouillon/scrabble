a499eca5c12ccbb83697ff2ad562199c9c2c83c9
Cyril Houillon <houillon.cyril@gmail.com>
2018-11-13 23:55:03 +0100

    add test for searching the best word

--- src/test/java/scrabble/ScoreTest.java
+++ src/test/java/scrabble/ScoreTest.java
@@ -2,6 +2,12 @@ package scrabble;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class ScoreTest {
@@ -17,4 +23,16 @@ class ScoreTest {
 		assertEquals(8, score);
 	}
 
+	@Test
+	void verify_the_best_word_played() throws IOException, URISyntaxException {
+		List<String> playedWords = Files
+				.readAllLines(Paths.get(this.getClass().getClassLoader().getResource("playedwords.txt").toURI()));
+		List<String> dictionary = Files
+				.readAllLines(Paths.get(this.getClass().getClassLoader().getResource("dictionary.txt").toURI()));
+		
+		String bestWord = "";
+
+		assertEquals("whizzing", bestWord);
+		assertEquals(33, scrabble.computeScore(bestWord));
+	}
 }
