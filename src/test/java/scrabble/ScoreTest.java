980b8ff1ef76de788bbd309be1e9ba450f46e59b
Cyril Houillon <houillon.cyril@gmail.com>
2018-11-14 00:04:07 +0100

    extract files loading in specific method. store the values in a cache

--- src/test/java/scrabble/ScoreTest.java
+++ src/test/java/scrabble/ScoreTest.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.Test;
 class ScoreTest {
 
 	Scrabble scrabble = new Scrabble(Scorings.ENGLISH_SCORING);
+	List<String> dictionary;
+	List<String> playedwords;
 
 	@Test
 	void score_should_return_the_sum_of_each_letter_value() {
@@ -25,19 +27,39 @@ class ScoreTest {
 	}
 
 	@Test
-	void verify_the_best_word_played() throws IOException, URISyntaxException {
-		List<String> playedWords = Files
-				.readAllLines(Paths.get(this.getClass().getClassLoader().getResource("playedwords.txt").toURI()));
-		List<String> dictionary = Files
-				.readAllLines(Paths.get(this.getClass().getClassLoader().getResource("dictionary.txt").toURI()));
+	void verify_the_best_word_played() {
 
-		String bestWord = playedWords.stream()//
-				.filter(word -> dictionary.contains(word))//
+		String bestWord = getPlayedWords().stream()//
+				.filter(word -> getDictionary().contains(word))//
 				.sorted(comparingInt(scrabble::computeScore).reversed())//
 				.findFirst()//
 				.get();
 
 		assertEquals("whizzing", bestWord);
 		assertEquals(33, scrabble.computeScore(bestWord));
+	}
+
+	private List<String> getDictionary() {
+		if (dictionary == null) {
+			try {
+				dictionary = Files.readAllLines(
+						Paths.get(this.getClass().getClassLoader().getResource("dictionary.txt").toURI()));
+			} catch (IOException | URISyntaxException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return dictionary;
+	}
+
+	private List<String> getPlayedWords() {
+		if (playedwords == null) {
+			try {
+				playedwords = Files.readAllLines(
+						Paths.get(this.getClass().getClassLoader().getResource("playedwords.txt").toURI()));
+			} catch (IOException | URISyntaxException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return playedwords;
 	}
 }
